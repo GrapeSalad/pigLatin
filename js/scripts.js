4f869e56261b90fc73978c33d7f81af9bdf25615
David Wilson and Olena Kuchko <david+olena@>
2017-05-03 16:02:27 -0700

    fix basic if-else logic, add user input and have it communicate with back-end

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,8 @@
-// function getSplitString(string) {
-//   return array = string.split("");
-// }
-// function checkString(array) {
-//
-// }
 var unuseableNumbers = ["1", "2","3","4","5","6","7","8","9","0"];
 var unuseableCharacters = [".", ",","/","#","!","$","%","^","&","*",";",":","{","}","=","-","_","`","~","(",")"];
 var vowelArray = ["a","e","i","o","u"];
 
 var verified  = function (string, array) {
-  // debugger;
   var Bool;
   for (var i = 0; i < array.length; i++) {
     if (string.includes(array[i])) {
@@ -22,52 +15,43 @@ var verified  = function (string, array) {
   }
   return Bool;
 }
-
-var sentence = "quontins";
-// var isBool = isVerified(test);
-// console.log(isBool);
-
-if (verified(sentence, unuseableNumbers)) {//not a string
-  console.log("improper characters, please remove number");
-} else {//is a string
-  console.log("contains appropriate letters")
- if (verified(sentence.charAt(0), vowelArray)) {//first character is vowel
-   console.log("the first character is a vowel");
- } else {// first letter is a consonant
-   console.log("first letter is consonant");
-   if (verified(sentence.charAt(1), vowelArray)){//second letter is vowel
-     console.log("second letter is a vowel");
-     if((sentence.charAt(0) === "q") && (sentence.charAt(1) === "u")) {
-       console.log("starts with qu");
-     }
-   } else {//second letter is a consonant
-     console.log("second letter is a consonant");
-   }
- }
+function addWay(string) {
+  return string+"way";
+}
+function multiLetterSliceAy(string, place0, place1, place2){
+  return (string.slice(place0) + string.slice(place1, place2) + "ay");
 }
 
-if ("not a str") {
 
-}
-else //if string
-{
-  if ("vowel") {
-
-  } else {//con
-    if ("second is vowel") {
-
-    }else {
-      //second is consonant
+var ifTests = function(sentence, unuseableNumbers, vowelArray){
+  var str;
+  if (verified(sentence, unuseableNumbers)) {//not a string
+    console.log("improper characters, please remove number");
+  } else {//is a string
+    console.log("contains appropriate letters")
+    if (verified(sentence.charAt(0), vowelArray)) {//first character is vowel
+     str = addWay(sentence);
     }
+    else if ((sentence.charAt(0) === "q") && (sentence.charAt(1) === "u")){
+      str = multiLetterSliceAy(sentence, 2, 0, 2);
+    }
+    else if (verified(sentence.slice(0,2), vowelArray)){// if second letter is vowel
+     str = multiLetterSliceAy(sentence, 1, 0, 1);
+    }
+    else if (verified(sentence.slice(0,3), vowelArray)){// if the third letter is vowel
+     str = multiLetterSliceAy(sentence, 2, 0, 2);
+    }
+    else {
+      str = multiLetterSliceAy(sentence, 3, 0, 3);// ONLY the first letter is a consonant
+   }
   }
+  return str;
+};
+//USER INTERFACE LOGIC
+$(function(){
+  $("#form").submit(function(event){
+    event.preventDefault();
 
-}
-
-
-// if (splitString.indexOf(unuseableNumbers) === -1) {
-//   var returnTest = "Im so confused";
-//   return true;
-//   console.log(returnTest);
-// } else {
-//   return false;
-// }
+    console.log(ifTests($("input#userString").val(), unuseableNumbers, vowelArray));
+  });
+});
